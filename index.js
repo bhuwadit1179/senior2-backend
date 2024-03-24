f8af68e9f16d5e4039ddf984a6b9aea7b992f805
Bhuwadit Athikraikun <bhuwaditathikraikun@Bhuwadits-MacBook-Pro.local>
2024-03-24 15:53:25 +0700

    deployment

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const app = require("./server.js");
+const express = require("express");
+const route = require("./routes/route.js");
+const cors = require("cors");
 
-const PORT = process.env.PORT || 3001;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", route);
+
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("test deployment");
+});
+
+module.exports = app;
